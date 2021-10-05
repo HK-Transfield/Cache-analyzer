a514622b230b4b1c2f670e8d19cdc0b87c1b7d6f
HK Transfield <h.k.transfield1@gmail.com>
2021-10-05 15:17:57 +1300

    Add all files

--- LLCEvaluator.java
+++ LLCEvaluator.java
@@ -7,38 +7,36 @@ public class LLCEvaluator {
     // static byte[] arr;
 
     /* hardcoded array sizes ranging from 1kb - 11mb */
-    final int[] sizes = { 1 * KB, 4 * KB, 8 * KB, 16 * KB, 32 * KB, 64 * KB, 128 * KB, 192 * KB, 256 * KB, 512 * KB,
-            1 * MB, 2 * MB, 3 * MB, 4 * MB, 8 * MB, 16 * MB, 32 * MB, 64 * MB, 128 * MB };
+    final int[] sizes = { 1 * KB, 4 * KB, 8 * KB, 16 * KB, 32 * KB, 64 * KB, 128 * KB, 256 * KB, 512 * KB, 1 * MB,
+            2 * MB, 4 * MB, 8 * MB, 16 * MB, 32 * MB, 64 * MB, 128 * MB, 256 * MB };
 
     public void testLLC() {
         int steps = 64 * 1024 * 1024; // arbitrary large number of steps
-        byte[] arr = new byte[256 * 1024 * 1024];
+        byte[] arr;
         int lengthMod;
-        long startTime, endTime;
-        Random rnd = new Random();
-
-        Arrays.fill(arr, (byte) 1);
+        long startTime, endTime, average;
+        // Random rnd = new Random();
 
         // the program creates a new array for eeach size defined
         for (int i = 0; i < sizes.length; i++) {
-            lengthMod = sizes[i] - 1;
+            arr = new byte[sizes[i]];
+            Arrays.fill(arr, (byte) 1);
+            average = 0;
 
-            /*
-             * Begin recording access times for each array size we are evaluating. It will
-             * the array an arbitrary number of steps.
-             */
-            startTime = System.nanoTime();
+            for (int j = 0; j <= 50; j++) {
+                startTime = System.nanoTime();
 
-            // increment a random 64th byte to modify the cache line.
-            // if it passes the last value if will loop back to the
-            // beginning
-            for (int s = 0; s < steps; s++) {
-                arr[(s * 64) & lengthMod]++;
-                // arr[(rnd.nextInt() * 64) & lengthMod]++;
+                lengthMod = arr.length - 1;
+                for (int s = 0; s < steps; s++) {
+                    arr[(s * 64) & lengthMod]++;
+                    // arr[(rnd.nextInt() * 64) & lengthMod]++;
+                }
+                endTime = System.nanoTime();
+
+                average += endTime - startTime;
             }
-            endTime = System.nanoTime();
 
-            System.out.println(((endTime - startTime)));
+            System.out.println((average / 50));
         }
     }
 }
--- llcEval.cpp
+++ llcEval.cpp
@@ -8,28 +8,24 @@
 using namespace std;
 using namespace std::chrono;
 
-int main(int argc, char *argv[])
+int main()
 {
-    static char *arr = new char[256 * 1024 * 1024]();
+    volatile static char *arr = new char[256 * 1024 * 1024]();
     const unsigned int steps = 64 * 1024 * 1024; // abitrary large number of steps
     const int sizes[] = {1 * KB, 4 * KB, 8 * KB, 16 * KB, 32 * KB, 64 * KB, 128 * KB, 192 * KB, 256 * KB, 512 * KB,
                          1 * MB, 2 * MB, 3 * MB, 4 * MB, 8 * MB, 16 * MB, 32 * MB, 64 * MB, 128 * MB};
     int lengthMod;
 
+    for (int i = 0; i < sizeof(arr); i++)
+        arr[i] = 'c';
+
     // the program creates a new array for eeach size defined
     for (int i = 0; i < sizeof(sizes) / sizeof(int); i++)
     {
-        /*
-        Begin recording access times for each array size we are evaluating.
-        It will the array an arbitrary number of steps.
-        */
+
         auto start = high_resolution_clock::now();
 
-        lengthMod = sizes[i] - 1;
-        /*
-        This for loop increments every 16th integer to modify
-        the 16 byte cache line (int = 4 bytes)
-        */
+        lengthMod = sizes[i] / sizeof(int) - 1;
         for (int s = 0; s < steps; s++)
             arr[(s * 64) & lengthMod]++;
 
