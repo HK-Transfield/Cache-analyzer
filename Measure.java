ec3a810db49274a8a87d59c7ff4856f4d2a72d9b
HK Transfield <h.k.transfield1@gmail.com>
2021-10-06 17:47:25 +1300

    Remove random for testing on other machines

--- Measure.java
+++ Measure.java
@@ -1,10 +1,9 @@
 import java.util.Arrays;
-import java.util.Random;
 
 class LLCEvaluator {
+    final int RUN_TIMING = 50;
     final int KB = 1024;
     final int MB = 1024 * KB;
-    // static byte[] arr;
 
     /* hardcoded array sizes ranging from 1kb - 11mb */
     final int[] sizes = { 1 * KB, 4 * KB, 8 * KB, 16 * KB, 32 * KB, 64 * KB, 128 * KB, 256 * KB, 512 * KB, 1 * MB,
@@ -12,14 +11,12 @@ class LLCEvaluator {
 
     public void testLLC() {
         int steps = 64 * 1024 * 1024; // arbitrary large number of steps
-        byte[] arr;
         int lengthMod;
         long startTime, endTime, average;
-        // Random rnd = new Random();
 
         // the program creates a new array for eeach size defined
         for (int i = 0; i < sizes.length; i++) {
-            arr = new byte[sizes[i]];
+            byte[] arr = new byte[sizes[i]];
             Arrays.fill(arr, (byte) 1);
             average = 0;
 
@@ -29,7 +26,6 @@ class LLCEvaluator {
                 lengthMod = arr.length - 1;
                 for (int s = 0; s < steps; s++) {
                     arr[(s * 64) & lengthMod]++;
-                    // arr[(rnd.nextInt() * 64) & lengthMod]++;
                 }
                 endTime = System.nanoTime();
 
